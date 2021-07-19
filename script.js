f17d36f1feb1efd679339059330e98d9c875d5ec
Ilija Nikolic <ilijan02gmail.com>
2021-07-19 17:32:59 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2FraTMyMSIsImEiOiJja3JhcGQ3azczajIwMnVtbnF4eTBkZXl1In0.VuJCFGds7BwmKwS7hyOAOA';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});
+
+// Zasto mi ovo ne radi :(
